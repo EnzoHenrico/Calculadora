b39fcde8475693081c47b90c106176eb6023a43c
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-19 00:39:50 -0300

    Add project files.

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿float num1, num2;
+
+Console.Write("Digite o primeiro numero: ");
+num1 = float.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo numero: ");
+num2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Soma : {num1 + num2}");
+Console.WriteLine($"Subtração: {num1 - num2}");
+Console.WriteLine($"Multiplicação: {num1 * num2}");
+
+if (num2 <= 0)
+{
+    Console.WriteLine("Impossivel divir por 0");
+}
+else
+{
+    Console.WriteLine($"Divisão: {num1 / num2}");
+}
